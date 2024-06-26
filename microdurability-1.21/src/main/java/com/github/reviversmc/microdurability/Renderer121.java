3775ec41f334d6655084f691d0ed67de4f7b51db
NebelNidas <nebelnidas@gmail.com>
2024-06-26 11:58:15 +0200

    Simplify and future-proof Mending detection on 1.21+

--- microdurability-1.21/src/main/java/com/github/reviversmc/microdurability/Renderer121.java
+++ microdurability-1.21/src/main/java/com/github/reviversmc/microdurability/Renderer121.java
@@ -1,31 +1,12 @@
 package com.github.reviversmc.microdurability;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.enchantment.Enchantment;
+import net.minecraft.component.EnchantmentEffectComponentTypes;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.world.World;
 
 public class Renderer121 extends Renderer1205 {
-	private RegistryEntry<Enchantment> mending;
-	private World world;
-
 	@Override
 	protected boolean hasMending(ItemStack stack) {
-		return EnchantmentHelper.getLevel(getMending(), stack) > 0;
-	}
-
-	private RegistryEntry<Enchantment> getMending() {
-		MinecraftClient client = MinecraftClient.getInstance();
-
-		if (client.world != world) {
-			world = client.world;
-			mending = world.getRegistryManager().get(RegistryKeys.ENCHANTMENT).getEntry(Enchantments.MENDING.getValue()).orElseThrow();
-		}
-
-		return mending;
+		return EnchantmentHelper.getEffect(stack, EnchantmentEffectComponentTypes.REPAIR_WITH_XP).isPresent();
 	}
 }
